d7768416ae7896171f043f99f6d67320d166a101
Tejashwini Bhendarkar <tmb22052005@gmail.com>
2025-03-07 20:47:38 +0530

    Objects

--- 02_Arrays/04_Objects.js
+++ 02_Arrays/04_Objects.js
@@ -11,7 +11,7 @@ tinderUser1.isLoggedIn = false;
 console.log(tinderUser1);
 
 const regularUser = {
-    email:"abc@gamul.com",
+    email:"abc@gamail.com",
     fullname : {
         userfullname : {
             firstname:"tony",
@@ -24,3 +24,47 @@ console.log(regularUser.fullname.userfullname);
 
 const obj1={1:"a" , 2: "b"};
 const obj2 ={3:"a" , 4: "b"};
+const obj3 = Object.assign({},obj1 , obj2);
+console.log(obj3);
+//it is used to merge 
+//{}->target  obj1 & obj2->value
+
+const obj4 ={ ...obj1 , ...obj2};
+console.log(obj4);
+//used to merge and mostly used
+
+const user = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 2,
+        email:"xyz@gmail.com"
+    }
+
+]
+console.log(user[1].email);
+
+console.log(Object.keys(tinderUser1));
+//[ 'id', 'name', 'isLoggedIn' ] return  array of keys
+console.log(Object.values(tinderUser1));
+//[ '123@abc', 'james', false ]
+console.log(Object.entries(tinderUser1));
+//[ [ 'id', '123@abc' ], [ 'name', 'james' ], [ 'isLoggedIn', false ] ]  
+console.log(tinderUser1.hasOwnProperty('isLoggedIn'));
+
+
+//***DESTRUCTURING OBJECT*****/
+
+const course = {
+    coursename :"JavaScript",
+    price:"66666",
+    location:"pune"
+}
+const {price} = course;
+console.log(price);
+
+const {coursename : cn} = course;
+console.log(cn);
+
